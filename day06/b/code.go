3aa045414aeb4a7455f91200aa4e85dd1ae05c6a
eric-create <>
2023-03-04 17:53:47 +0100

    Day 6: Finished.

---
+++ day06/b/code.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	lines := ReadLines("./input.txt")
+	for _, line := range lines {
+		fmt.Println(GetMarkerEnd(line, 4))
+		fmt.Println(GetMarkerEnd(line, 14))
+		fmt.Println("---")
+	}
+}
+
+func GetMarkerEnd(line string, markerLen int) int {
+	series := []rune(line)
+	markerEnd := 0
+
+	for i := 0; ; i++ {
+		pool := series[i : i+markerLen]
+		duplicatePoint := GetDuplicatePoint(pool)
+
+		if duplicatePoint == 0 {
+			markerEnd = i + (markerLen - 1)
+			break
+		}
+
+		// We want to preceed from the position behind the first element of the duplicate.
+		i += duplicatePoint - 1
+		if i+markerLen > len(series) {
+			return 0
+		}
+	}
+
+	return markerEnd + 1
+}
+
+func GetDuplicatePoint(pool []rune) int {
+	visited := make(map[rune]int, 0)
+	for i := 0; i < len(pool); i++ {
+		if visited[pool[i]] > 0 {
+			return visited[pool[i]]
+		} else {
+			visited[pool[i]] = i + 1
+		}
+	}
+	return 0
+}
+
+func ReadLines(path string) []string {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return strings.Split(string(content), "\n")
+}

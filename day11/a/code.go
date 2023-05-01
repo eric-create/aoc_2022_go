fd2e17d82bd5f6f39a6db681c0325ae5cb0c7f90
eric-create <>
2023-05-01 20:56:07 +0200

    Day 11: Finished task A.

--- day11/a/code.go
+++ day11/a/code.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -12,11 +13,52 @@ func main() {
 	// Appending an empty string to the array is important, so that the last monkey defined
 	// in the text file is being processed.
 	lines = append(lines, "")
-	GetMonkeys(lines)
+	monkeys := GetMonkeys(lines)
+	rounds := 20
+
+	for i := 1; i <= rounds; i++ {
+		for _, monkey := range monkeys {
+			for len(monkey.Items) > 0 {
+				item := monkey.PopItem()
+				item = monkey.Inspect(item)
+				test := monkey.Test(item)
+				peer := monkey.GetPeer(test)
+				monkey.Throw(item, peer, monkeys)
+			}
+		}
+	}
+
+	for i, monkey := range monkeys {
+		fmt.Println("Monkey", i, "Items", monkey.Items)
+	}
+	fmt.Println()
+	business := GetMonkeyBusiness(monkeys)
+	fmt.Println()
+	fmt.Println("Monkey Business", business)
 }
 
-func GetMonkeys(lines []string) []Monkey {
-	monkeys := []Monkey{}
+func GetMonkeyBusiness(monkeys []*Monkey) int {
+	businesses := GetBusinesses(monkeys)
+	monkeyCount := len(monkeys)
+	return businesses[monkeyCount-1] * businesses[monkeyCount-2]
+}
+
+func GetBusinesses(monkeys []*Monkey) []int {
+	businesses := []int{}
+
+	for i, monkey := range monkeys {
+		businesses = append(businesses, monkey.Business)
+
+		fmt.Println("Business of monkey", i, "is: ", businesses[i])
+	}
+
+	sort.Ints(businesses)
+
+	return businesses
+}
+
+func GetMonkeys(lines []string) []*Monkey {
+	monkeys := []*Monkey{}
 	monkeyLines := []string{}
 
 	for _, line := range lines {
@@ -30,9 +72,9 @@ func GetMonkeys(lines []string) []Monkey {
 	return monkeys
 }
 
-func GetMonkey(lines []string) Monkey {
+func GetMonkey(lines []string) *Monkey {
 
-	monkey := Monkey{}
+	monkey := &Monkey{Business: 0}
 
 	for _, line := range lines {
 		if strings.Contains(line, "Starting items") {
@@ -48,8 +90,6 @@ func GetMonkey(lines []string) Monkey {
 		}
 	}
 
-	fmt.Println(monkey.Items, monkey.TestingDivisor, monkey.PeerTrue, monkey.PeerFalse)
-	fmt.Println()
 	return monkey
 }
 
@@ -65,14 +105,29 @@ func GetStartingItems(line string) []int {
 
 func GetOperation(line string) Operation {
 	operationSequence := string(line[23:])
-	operand, _ := strconv.Atoi(operationSequence[2:])
+	operator := operationSequence[0]
+	operand := operationSequence[2:]
 
 	// Looking at the input text then it turns out that the only mathematical operations
 	// applied are multiplication and addition.
-	if operationSequence[0] == '+' {
-		return func(old int) int { return old + operand }
+	if operator == '+' {
+		return func(old int) int {
+			if operand == "old" {
+				return old + old
+			} else {
+				constant, _ := strconv.Atoi(operand)
+				return old + constant
+			}
+		}
 	} else {
-		return func(old int) int { return old * operand }
+		return func(old int) int {
+			if operand == "old" {
+				return old * old
+			} else {
+				constant, _ := strconv.Atoi(operand)
+				return old * constant
+			}
+		}
 	}
 }
 
@@ -100,17 +155,36 @@ type Monkey struct {
 	TestingDivisor int
 	PeerTrue       int
 	PeerFalse      int
+	Business       int
 }
 
 func (monkey *Monkey) PopItem() int {
+	monkey.Business++
 	item := monkey.Items[0]
 	monkey.Items = monkey.Items[1:]
 	return item
 }
 
-func (monkey *Monkey) Inspect() {
-	item := monkey.PopItem()
+func (monkey *Monkey) Inspect(item int) int {
 	item = monkey.operation(item)
+	item = item / 3
+	return item
+}
+
+func (monkey *Monkey) Test(item int) bool {
+	return item%monkey.TestingDivisor == 0
+}
+
+func (monkey *Monkey) GetPeer(test bool) int {
+	if test {
+		return monkey.PeerTrue
+	} else {
+		return monkey.PeerFalse
+	}
+}
+
+func (monkey *Monkey) Throw(item int, peer int, monkeys []*Monkey) {
+	monkeys[peer].Items = append(monkeys[peer].Items, item)
 }
 
 func ReadLines(path string) []string {
